c0bb7aa530f11b6eae41c20f3b397fb43e5b750d
trinadhtt <thrinadh897@gmail.com>
2026-09-10 22:38:52 +0530

    Api gateway changes

---
+++ ecom-api-gateway/src/test/java/com/ecom/ecomapigateway/EcomApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
